c6fab8d3e2d8d695d8228062b0ef1eafd9b3f445
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2025-07-21 16:35:29 +0530

    gkj

--- src/app/image-slider/image-slider.component.ts
+++ src/app/image-slider/image-slider.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, HostListener, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-image-slider',
@@ -6,77 +6,82 @@ import { AfterViewInit, Component, ElementRef, HostListener, QueryList, ViewChil
   templateUrl: './image-slider.component.html',
   styleUrl: './image-slider.component.css'
 })
-export class ImageSliderComponent implements AfterViewInit {
-  @ViewChild('slider') slider!: ElementRef;
-  @ViewChildren('captionText') captionTextRefs!: QueryList<ElementRef>;
-
-  images = [
-    { src: 'assets/us 1.jpeg', message: 'Our first photo together! 💝',type:'image' },
-    { src: 'assets/us 2.jpeg', message: 'Rakhi 2K24 💝',type:'image' },
-    { src: 'assets/solo 1.jpeg', message: '30-Jan-2k25' ,type:'image'},
-    {src:'assets/solo 2.jpeg', message:'sundar 💝',type:'image'},
-    {src:'assets/us 3.jpeg', message:'Best 💯🔥 ',type:'image'},
-     {src:'assets/us 4.jpeg', message:'Smart to hu mai 😂 ',type:'image'},
-    {src:'assets/solo 3.jpeg', message:'cute 😺',type:'image'},
-    {src:'assets/solo 8.jpeg', message:'Mathura 2k23 😂😂', type:'image'},
-     {src:'assets/us 10.jpeg', message:'10-jul-2k23💝 ',type:'image'},
- {src:'assets/solo 4.jpeg', message:'Fav  Atisundar 💝',type:'image'},
-    { src: 'assets/us 5.jpeg', message: 'Rakhi 2k23' ,type:'image'},
-    { src: 'assets/us 6.jpeg', message: 'Diwali 2k24 💝' ,type:'image'},
-     {src:'assets/aaloo.jpeg', message:'Aaloo 🥔 ',type:'image'},
-      {src:'assets/solo 9.jpeg', message:'chuiya 🐁', type:'image'},
- {src:'assets/solo 5.jpeg', message:'16-june-2k25',type:'image'},
- { src: 'assets/us 11.jpeg', message: 'U with ur handsome bhai 💝' ,type:'image'},
-  {src:'assets/solo 6.jpeg', message:'16-june-2k25',type:'image'},
-   {src:'assets/solo 10.jpeg', message:'cute 😺', type:'image'},
-  {src:'assets/solo 7.jpeg', message:'cute 😺', type:'image'},
-  { src: 'assets/us 7.jpeg', message: 'feb 2k23' ,type:'image'},
-  {src:'assets/last.jpeg', message:'Happy birthday Bhindi 🎂',type:'image'},
+export class ImageSliderComponent implements OnInit, OnDestroy {
+  testimonials = [
+  {src:'assets/1.jpeg',message:'💖'},
+  {src:'assets/2.jpeg',message:'💖'},
+  {src:'assets/3.jpeg',message:'💖'},
+  {src:'assets/4.jpeg',message:'💖'},
+  {src:'assets/5.jpeg',message:'💖'},
+  {src:'assets/6.jpeg',message:'😼😼'},
+  {src:'assets/7.jpeg',message:'💖'},
+  {src:'assets/8.jpeg',message:'💖'},
+  {src:'assets/9.jpeg',message:'💖'},
+  {src:'assets/10.jpeg',message:'💖'},
   ];
 
-  currentIndex = 0;
+  currentSlide = 0;
+  interval: any;
 
-  ngAfterViewInit() {
-    this.updateSlide();
-    setTimeout(() => this.typeCaption(), 0); // Wait for view to init
+  ngOnInit() {
+    this.startAutoplay();
   }
 
-  nextSlide() {
-    this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    this.updateSlide();
-    setTimeout(() => this.typeCaption(), 50); // Wait for DOM to update
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 
-  prevSlide() {
-    this.currentIndex =
-      (this.currentIndex - 1 + this.images.length) % this.images.length;
-    this.updateSlide();
-    setTimeout(() => this.typeCaption(), 50); // Wait for DOM to update
+ nextSlide() {
+  this.currentSlide = (this.currentSlide + 1) % this.testimonials.length;
+  this.resetAutoplay();
+}
+
+prevSlide() {
+  this.currentSlide = (this.currentSlide - 1 + this.testimonials.length) % this.testimonials.length;
+  this.resetAutoplay();
+}
+
+goToSlide(index: number) {
+  this.currentSlide = index;
+  this.resetAutoplay();
+}
+resetAutoplay() {
+  clearInterval(this.interval); // stop current interval
+  this.startAutoplay();         // start new interval
+}
+
+
+  startAutoplay() {
+    this.interval = setInterval(() => {
+      this.nextSlide();
+    }, 7000);
   }
 
-  updateSlide() {
-    const slideWidth = window.innerWidth;
-    this.slider.nativeElement.style.transform = `translateX(-${
-      this.currentIndex * slideWidth
-    }px)`;
+  touchStartX = 0;
+touchEndX = 0;
+
+handleTouchStart(event: TouchEvent) {
+  this.touchStartX = event.changedTouches[0].screenX;
+}
+
+handleTouchEnd(event: TouchEvent) {
+  this.touchEndX = event.changedTouches[0].screenX;
+  this.handleSwipeGesture();
+}
+
+handleSwipeGesture() {
+  const deltaX = this.touchEndX - this.touchStartX;
+
+  // Minimum swipe distance threshold
+  const minSwipeDistance = 50;
+
+  if (deltaX > minSwipeDistance) {
+    // Swiped right → previous slide
+    this.prevSlide();
+  } else if (deltaX < -minSwipeDistance) {
+    // Swiped left → next slide
+    this.nextSlide();
   }
-
-  typeCaption() {
-    const captions = this.captionTextRefs.toArray();
-    const currentCaptionEl = captions[this.currentIndex].nativeElement;
-
-    const message = this.images[this.currentIndex].message;
-    let i = 0;
-    currentCaptionEl.textContent = ''; // Clear previous
-
-    const interval = setInterval(() => {
-      if (i < message.length) {
-        currentCaptionEl.textContent += message.charAt(i);
-        i++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 50);
-  }
+}
 
 }
